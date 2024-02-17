a2946c31b0cee9cbd0f521ec774e7c29493d4465
Andres Perez <75587267+andypc93@users.noreply.github.com>
2024-02-17 17:23:16 -0400

    commit

--- .venv/app.py
+++ .venv/app.py
@@ -3,9 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def main_menu():
-    # Render the 'main_menu.html' template when someone visits the root URL "/"
+def home():
     return render_template('main_menu.html')
+
+@app.route('/about')
+def about():
+    return "<h1>About Page</h1>"
+
+@app.route('/services')
+def services():
+    return "<h1>Services Page</h1>"
+
+@app.route('/contact')
+def contact():
+    return "<h1>Contact Us</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
