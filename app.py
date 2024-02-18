b4b9a95bf3eb0d7ba231d727a1c8954b472e6835
Andres Perez <75587267+andypc93@users.noreply.github.com>
2024-02-17 20:35:10 -0400

    front_end push

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+""" @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html') """
+
+@app.route('/')
+def main_menu():
+    return render_template('main_menu.html')
+@app.route('/')
+def menu():
+    return render_template('main_menu.html')
 
 if __name__=="__main__":
     app.run(debug=True)
