8d70454c30cbd5edc5fb32015e8f530e37339368
Andres Perez <75587267+andypc93@users.noreply.github.com>
2024-02-19 18:04:49 -0400

    Delete cs_venv directory

--- cs_venv/Scripts/get_venv_path.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Get the directory of the current script (activate.bat)
-script_dir = os.path.dirname(os.path.realpath(__file__))
-
-# Construct the path to the virtual environment directory
-venv_path = os.path.abspath(os.path.join(script_dir, '..', '..', 'cs_venv'))
-
-print(venv_path)
